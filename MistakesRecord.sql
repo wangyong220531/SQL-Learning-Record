ea97bebd8bbaf6a41c0d5cd08d5557a53011a4e3
wangyong220531 <wangyong220531@gmail.com>
2023-12-27 01:29:48 +0800

    add MistakesRecord.sql file

---
+++ MistakesRecord.sql
@@ -0,0 +1,7 @@
+-- 1、通过 SQL，您如何从 "Persons" 表中选取 "FirstName" 列的值以 "a" 开头的所有记录？
+-- 正确答案：SELECT * FROM Persons WHERE FirstName LIKE 'a%'
+
+-- 2、通过 SQL，您如何按字母顺序选取 Persons 表中 LastName 介于 Adams 和 Carter 的所有记录？
+-- 正确答案：SELECT * FROM Persons WHERE LastName BETWEEN 'Adams' AND 'Carter'
+
+
